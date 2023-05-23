f744600539ed6e140a27e6074b6c6349d12d41af
MatiGuar <matiasguarnaccia@outlook.es>
2023-05-23 16:55:25 -0300

    desafío 2

--- product.js
+++ product.js
@@ -13,14 +13,21 @@ class ProductManager {
 
     getProducts(){
         
-        if(fs.existsSync(this.#path)){
-            const dataProduct = fs.readFileSync(this.#path, 'utf-8')
-            this.#products = JSON.parse(dataProduct)
-            console.log(this.#products) 
-        } else {
-            fs.writeFileSync(this.#path, JSON.stringify(this.#products))
-            console.log(this.#products) 
+        try {
+            let data
+            if(fs.existsSync(this.#path)){
+                const dataProduct = fs.readFileSync(this.#path, 'utf-8')
+                this.#products = JSON.parse(dataProduct)
+                data =  this.#products 
+            } else {
+                fs.writeFileSync(this.#path, JSON.stringify(this.#products))
+                data = this.#products 
+            }
+            return data
+        } catch (error) {
+            console.log(error)
         }
+        
 
         
     }
@@ -45,7 +52,12 @@ class ProductManager {
             console.log('El codigo del producto que desea agregar ya existe.')
         } else {
             this.#products.push(product)
-            fs.writeFileSync(this.#path, JSON.stringify(this.#products))
+            try {
+                fs.writeFileSync(this.#path, JSON.stringify(this.#products))
+            } catch (error) {
+                console.log(error)
+            }
+          
         }
 
         
@@ -54,15 +66,16 @@ class ProductManager {
    
 
         getProductById(id){
-         this.#products.find((e)=>{
-            return e.id == id
-        }) || console.error("Not Found")
+         const productById = this.#products.find(e=> e.id === id) 
+
+         return productById ? productById : "Not found"
     }   
     
 
     updateProduct(id, campo, valor){
        
-        const product = this.#products.find((product) => product.id === id)
+        try {
+            const product = this.#products.find((product) => product.id === id)
 
         if(!product){
             return `No existe el producto con ID: ${id}`
@@ -74,24 +87,50 @@ class ProductManager {
             product[campo] = valor
         }
         fs.writeFileSync(this.#path, JSON.stringify(this.#products))
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    deleteProduct(id) {
+        
+        try {
+            const indexId = this.#products.findIndex(product => product.id === id)
+        
+        if (indexId >= 0){
+            this.#products.splice(indexId, 1)
+            fs.writeFileSync(this.#path, JSON.stringify(this.#products))
+            return `El producto con id: ${id} ha sido eliminando correctamente`
+        } else {
+            return `No existe ningun producto con id: ${id}`
+        }
+        } catch (error) {
+            console.log(error)
+        }
+        
     }
 
 }
 
 const product = new ProductManager() 
 
-console.log(product.getProducts())
+/* console.log(product.getProducts()) */
 
 product.addProduct("Caramelo", "Dulce", 20, "thumbnail1", "12F24R", 100)
 product.addProduct("Jugo", "Manzana", 160, "thumbnail1", "12JG44M", 100)
 product.addProduct("Alfajor", "Chocoalte", 140, "thumbnail1", "12AFJ54C", 100)  
+product.addProduct("Sandwich", "Tomtae", 200, "thumbnail1", "12SDJ4T", 100)  
+product.addProduct("Gaseosa", "Cola", 150, "thumbnail1", "12GSA95C", 100)  
 
 console.log(product.getProducts())
 
-
-
 console.log("producto por id", product.getProductById(1))
 
-product.updateProduct(2,"description", "Agridulce")
+product.updateProduct(2,"description", "Agridulce")  
+console.log(product.getProducts())
+
+console.log(product.deleteProduct(3))
+console.log(product.deleteProduct(10)) 
 
 console.log(product.getProducts()) 
